e1cbf22a67b770450f76b4f471658c4e94179435
yaska258 <121067455+yaska258@users.noreply.github.com>
2023-03-29 08:28:38 -0500

    added rg name on VNET

--- VNET/main.tf
+++ VNET/main.tf
@@ -74,7 +74,7 @@ resource "azurerm_linux_virtual_machine" "azureproject" {
 
 data "azurerm_public_ip" "azureproject" {
   name                = azurerm_public_ip.azureproject.name
-  resource_group_name = azurerm_virtual_machine.azureproject.resource_group_name
+  resource_group_name = azurerm_virtual_machine.azureproject_VNET.resource_group_name
 }
 
 output "public_ip_address" {
