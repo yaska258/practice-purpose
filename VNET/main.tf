3db7f6ee44db1fe90f66f110752751015fd08d61
yaska258 <121067455+yaska258@users.noreply.github.com>
2023-03-29 16:19:05 -0500

    removed a line

--- VNET/main.tf
+++ VNET/main.tf
@@ -17,19 +17,6 @@ resource "azurerm_subnet" "azureproject" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_public_ip" "azureproject" {
-  name                    = "azureproject-pip"
-  location                = azurerm_resource_group.azureproject_VNET.location
-  resource_group_name     = azurerm_resource_group.azureproject_VNET.name
-  allocation_method       = "Dynamic"
-  idle_timeout_in_minutes = 30
-
-  tags = {
-    environment = "test"
-  }
-}
-
-
 
 resource "azurerm_network_interface" "azureproject" {
   name                = "azureproject-nic"
