f5140b687bfba09c4bc3bb8147594132416b6615
yaska258 <121067455+yaska258@users.noreply.github.com>
2023-03-28 22:36:58 -0500

    added SS file

---
+++ SS/output.tf
@@ -0,0 +1,8 @@
+output ss_id {
+    value = azurerm_linux_virtual_machine_scale_set.azureproject.id
+}
+
+
+output ss_unique_id {
+    value = azurerm_linux_virtual_machine_scale_set.azureproject.unique_id
+}
